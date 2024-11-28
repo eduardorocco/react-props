0c3ef71e389ba37e4b84be0e645ea662bb333252
eduardorocco <eduardo_rocco@hotmail.it>
2024-11-28 15:58:47 +0100

    ADD first props

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,12 @@
 import style from './Card.module.css'
 
-export default function Card() {
+export default function Card({title = '',}) {
     return (
         <div className={style.card}>
             <div>
                 <img className={style.image} src="https://picsum.photos/600/400" alt="" />
                 <div className={style.contentContainer}>
-                    <div className={style.title}>Titolo del post</div>
+                    <div className={style.title}>{title}</div>
                     <div className={style.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat iure provident consectetur expedita natus quia doloribus asperiores beatae maxime reiciendis repellat numquam voluptate iusto corrupti quidem nulla placeat ut dicta et quo quod, rerum magni ab ipsum? Magnam delectus explicabo sunt! Consectetur hic debitis quo corrupti aliquam doloremque iste illo?</div>
                     <div className={style.button}>Leggi di più</div>
                 </div>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,17 @@
 import style from './Main.module.css'
 import Card from "../Card/Card"
+import { posts } from '../../../data/posts'
 
 export default function Main() {
     return (
         <main>
             <div className="container">
                 <div className={style.row}>
-                    <Card></Card>
+                    {posts.map((post) => (
+                        <div key={post.id} className="col-6">
+                            <Card title={post.title}/>
+                        </div>
+                    ))}
                 </div>
             </div>
         </main>
