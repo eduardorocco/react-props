8aa495dc9fcbbd0e79ee712be6abc183b7a4ef1a
eduardorocco <eduardo_rocco@hotmail.it>
2024-11-28 17:21:42 +0100

    ADD css style to tags

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,22 @@
 import style from './Card.module.css'
+import placeHolder from '../../assets/placeholder.webp'
 
-export default function Card({title = '', content = '', tags =[], image = ''}) {
+export default function Card({ title = '', content = '', tags = [], image = '' }) {
 
-    const description = tags.join(', ')
-    
 
     return (
         <div className={style.card}>
             <div>
-                <img className={style.image} src={image} alt="" />
+                <img className={style.image} src={image || placeHolder} alt="" />
                 <div className={style.contentContainer}>
                     <div className={style.title}>{title}</div>
-                    <div className={`${style.tags}`}>{description}</div>
+                    <div className={style.tags}>
+                        {tags.map((tag, index) => (
+                            <div key={index} className={`${style.tags}}`}>
+                                {tag}
+                            </div>
+                        ))}
+                    </div>
                     <div className={style.text}>{content}</div>
                     <div className={style.button}>Leggi di più</div>
                 </div>
